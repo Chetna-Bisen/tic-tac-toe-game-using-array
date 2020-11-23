2dd0dbf857aeb4a688eebff85204cafe8f5029e3
Chetna-Bisen <bisen.chetna044@gmail.com>
2020-11-23 20:43:16 +0530

    validating posiion

--- Tictactoe.java
+++ Tictactoe.java
@@ -4,6 +4,7 @@
  *
 */
 
+import java.net.StandardSocketOptions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -35,7 +36,7 @@ public class Tictactoe {
     }
 
 //displaying board
-    public static void displayBoard() {
+    public static void printGameBoard() {
         System.out.println(board.get(0).get(0)+ " | " +board.get(0).get(1)+ " | " +board.get(0).get(2));
         System.out.println("----------");
         System.out.println(board.get(1).get(0)+ " | " +board.get(1).get(1)+ " | " +board.get(1).get(2));
@@ -43,15 +44,31 @@ public class Tictactoe {
         System.out.println(board.get(2).get(0)+ " | " +board.get(2).get(1)+ " | " +board.get(2).get(2)+"\n");
     }
 
+//valid position
+    public static int checkValidPosition(int positionValid) {
+        int rowPosition = (int) Math.floor((positionValid-1)/3); 
+        int colPosition = (positionValid-1)%3;
+        String strPos = Integer.toString((positionValid));
+        if (board.get(rowPosition).get(colPosition).equals(strPos))
+            return 1;
+        else
+            return 0;
+    }
+
+
 //main method
     public static void main(String args[]){
         Tictactoe ticTacToe = new Tictactoe();
         ticTacToe.initBoard();
         Scanner choice = new Scanner(System.in);
-        System.out.println("Enter the Symbol Choice ");
+        System.out.println("Enter the Symbol Choice  between 1 and 2 ");
         int symChoice = choice.nextInt();
         ticTacToe.chooseSymbol(symChoice);
-        ticTacToe.displayBoard();
+        ticTacToe.printGameBoard();
+        Scanner validPos = new Scanner(System.in);
+        System.out.println("Enter the valid position between 1 to 9 ");
+        int validPosition = validPos.nextInt();
+        ticTacToe.checkValidPosition(validPosition);
 
 
     }
