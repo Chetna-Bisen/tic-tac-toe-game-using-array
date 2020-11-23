dfc221a701d42ed0cd675d314f7cba33bdb2e829
Chetna-Bisen <bisen.chetna044@gmail.com>
2020-11-23 20:14:52 +0530

    Symbol selection

--- Tictactoe.java
+++ Tictactoe.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 
 public class Tictactoe {
     private static ArrayList<ArrayList<String> > board;
+    private static String playerSymbol = " ";
+    private static String cpuSymbol = " ";
 
     //board to be initialized
     public void initBoard(){
@@ -11,5 +13,18 @@ public class Tictactoe {
         board.add( new ArrayList<String>(Arrays.asList(" "," "," ")));
         board.add( new ArrayList<String>(Arrays.asList(" "," "," ")));
         }
+
+//Symbol Selection
+    public static void chooseSymbol(int userSymbolChoice){
+        if (userSymbolChoice == 1){
+            playerSymbol = "X";
+            cpuSymbol  =  "O";
+        }
+        else{
+            playerSymbol = "O";
+            cpuSymbol = "X";
+        }
+
+    }
 }
 
